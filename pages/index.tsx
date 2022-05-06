d3ef72be0c3cdfde3a80ed7b0b045cb63eabac26
graphica99 <mendsalbert@gmail.com>
2022-05-06 14:26:00 +0000

    schain ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,8 +9,14 @@ const Index = (props: Props) => {
       <div className="font-Montserrat mx-6 relative">
         <div className="fixed top-0 left-0">
           <img src={"/images/navector.svg"} className="w-full " />
-          <div className="fixed mx-6 py-4 top-0 flex flex-row">
+          <div className="fixed mx-6 py-4 top-0 flex flex-row justify-between">
             <img src={"/images/logo.svg"} className="w-24" />
+            <div
+              onClick={() => {}}
+              className="  bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500  text-center w-full   px-6 py-3 rounded-full cursor-pointer text-white"
+            >
+              Connect
+            </div>
             {/* <p>fdsdf</p> */}
           </div>
         </div>
