4a7e61451fa32a795d063e3e8dd39ddf9f3fcc0f
graphica99 <mendsalbert@gmail.com>
2022-05-06 22:59:02 +0000

    schain ui

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -6,7 +6,9 @@ const Admin = (props: Props) => {
   return (
     <div className="font-Montserrat w-full">
       <div className="flex flex-row w-full">
-        <div className="bg-red-500 w-1/12 h-screen">fdsffs</div>
+        <div className="bg-gradient-to-b  from-[#0469A1] via-[#0469A1]  to-[#0C9FF2] w-1/12 h-screen">
+          fdsffs
+        </div>
         <div className="bg-green-500 w-full h-screen">fsdfsdf</div>
       </div>
     </div>
