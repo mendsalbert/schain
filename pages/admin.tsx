bee805c38f69b1e79020128a4d420f98662e2310
graphica99 <mendsalbert@gmail.com>
2022-05-06 23:13:39 +0000

    schain ui

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -5,12 +5,13 @@ type Props = {};
 
 const Admin = (props: Props) => {
   return (
-    <div className="font-Montserrat w-full">
+    <div className="font-Montserrat  w-full">
       <div className="flex flex-row w-full">
-        <div className="bg-gradient-to-b p-5 text-center from-[#0469A1] via-[#0469A1]  to-[#0C9FF2] w-1/12 h-screen">
-          <img src={"/images/logo.svg"} className="w-24 md:w-32" />
-          <div>
-            <ClipboardListIcon className="h-8" />
+        <div className="bg-gradient-to-b flex flex-col text-center items-center from-[#0469A1] via-[#0469A1]  to-[#0C9FF2]  h-screen">
+          {/* <img src={"/images/logo.svg"} /> */}
+          <div className="">
+            <ClipboardListIcon className="h-10 text-white" />
+            <p>Orders</p>
           </div>
         </div>
         <div className="bg-green-500 w-full h-screen">fsdfsdf</div>
