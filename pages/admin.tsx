469b78ed70e3961bd4677ed421c08e9b3d605ead
graphica99 <mendsalbert@gmail.com>
2022-05-06 23:34:28 +0000

    schain ui

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -40,7 +40,7 @@ const Admin = (props: Props) => {
                   <div className="hidden sm:flex w-full bg-gray-200 h-0.5 "></div>
                 </div>
                 <div className="mt-3 sm:pr-8">
-                  <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                  <h3 className="text-lg font-semibold text-gray-900 ">
                     Flowbite Library v1.0.0
                   </h3>
                   <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
@@ -71,7 +71,7 @@ const Admin = (props: Props) => {
                   <div className="hidden sm:flex w-full bg-gray-200 h-0.5 "></div>
                 </div>
                 <div className="mt-3 sm:pr-8">
-                  <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                  <h3 className="text-lg font-semibold text-gray-900 ">
                     Flowbite Library v1.2.0
                   </h3>
                   <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
@@ -102,7 +102,7 @@ const Admin = (props: Props) => {
                   <div className="hidden sm:flex w-full bg-gray-200 h-0.5 "></div>
                 </div>
                 <div className="mt-3 sm:pr-8">
-                  <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                  <h3 className="text-lg font-semibold text-gray-900 ">
                     Flowbite Library v1.3.0
                   </h3>
                   <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
