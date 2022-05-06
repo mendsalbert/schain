4fa84d50d2f8e39821cb221150894e6fe07aaf81
graphica99 <mendsalbert@gmail.com>
2022-05-06 14:19:33 +0000

    schain ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,20 @@ import React from "react";
 type Props = {};
 
 const Index = (props: Props) => {
-  return <div>index</div>;
+  return (
+    <div>
+      {/* layout */}
+      <div className="font-Montserrat mx-6 relative">
+        <div>
+          <img
+            src={"/images/navector.svg"}
+            className="w-full
+    "
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Index;
