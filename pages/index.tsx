78a621cd305b7a4edb050465c50f6c6b6910f4d6
graphica99 <mendsalbert@gmail.com>
2022-05-06 18:14:38 +0000

    schain ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -134,6 +134,17 @@ const Index = (props: Props) => {
             order, sit and watch the supply chain processes
           </p>
         </div>
+
+        <footer>
+          <div className="mt-28 text-center w-full py-4 flex flex-col justify-center items-center">
+            <p className="text-lg text-gray-500 ">Connet with us</p>
+            <div className="flex flex-row space-x-4 cursor-pointer">
+              <img src="/images/twitter.svg" className="w-7" />
+              <img src="/images/discord.svg" className="w-10" />
+              <img src="/images/instagram.svg" className="w-7" />
+            </div>
+          </div>
+        </footer>
       </div>
     </div>
   );
