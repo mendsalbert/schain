33a0a1754aeac99b3409b259335dfc37006bac1e
graphica99 <mendsalbert@gmail.com>
2022-05-06 15:53:29 +0000

    schain ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { ClipboardListIcon } from "@heroicons/react/outline";
 type Props = {};
 
 const Index = (props: Props) => {
@@ -55,8 +55,9 @@ const Index = (props: Props) => {
               <div className="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
                 <h1 className="mx-auto font-semibold text-lg text-white">1</h1>
               </div>
-              <div className="rounded-tl-full order-1 bg-gradient-to-r from-[#0C9FF2] via-[#0C9FF2] text-white to-[#136390] rounded-lg shadow-xl w-5/12  p-4">
-                <img src="/images/order.svg " className="w-14" />
+              <div className="rounded-tl-full rounded-bl-full order-1 bg-gradient-to-r from-[#0C9FF2] via-[#0C9FF2] text-white to-[#136390] rounded-lg shadow-xl w-5/12  p-4">
+                <ClipboardListIcon className="h-4" />
+                {/* <img src="/images/order.svg " className="w-14" /> */}
                 <h3 className="mt-2  text-white text-lg">Make Order</h3>
               </div>
             </div>
