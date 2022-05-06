8ea49e978d5a50cb009b8686db83dcd43365c87f
graphica99 <mendsalbert@gmail.com>
2022-05-06 23:33:20 +0000

    schain ui

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -30,23 +30,12 @@ const Admin = (props: Props) => {
             </button>
           </div>
 
-          <div>
+          <div className="mt-16">
             <ol className="items-center sm:flex">
               <li className="relative mb-6 sm:mb-0">
                 <div className="flex items-center">
-                  <div className="flex z-10 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
-                    <svg
-                      className="w-3 h-3 text-blue-600 dark:text-blue-300"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                        clip-rule="evenodd"
-                      ></path>
-                    </svg>
+                  <div className="flex z-10 justify-center items-center w-max h-max p-2 bg-green-400 rounded-full ring-0 ring-white ">
+                    <ClipboardListIcon className="h-8 text-white" />
                   </div>
                   <div className="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
                 </div>
@@ -65,7 +54,7 @@ const Admin = (props: Props) => {
               </li>
               <li className="relative mb-6 sm:mb-0">
                 <div className="flex items-center">
-                  <div className="flex z-10 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
+                  <div className="flex z-10 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-0 ring-white ">
                     <svg
                       className="w-3 h-3 text-blue-600 dark:text-blue-300"
                       fill="currentColor"
@@ -96,7 +85,7 @@ const Admin = (props: Props) => {
               </li>
               <li className="relative mb-6 sm:mb-0">
                 <div className="flex items-center">
-                  <div className="flex z-10 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
+                  <div className="flex z-10 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-0 ring-white ">
                     <svg
                       className="w-3 h-3 text-blue-600 dark:text-blue-300"
                       fill="currentColor"
