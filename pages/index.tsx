c48a07c8104831e4581ecb51a1e0e4e1b6ac4362
graphica99 <mendsalbert@gmail.com>
2022-05-06 14:32:58 +0000

    schain ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,8 +8,8 @@ const Index = (props: Props) => {
       {/* layout */}
       <div className="font-Montserrat mx-6 relative">
         <div className="fixed w-full top-0 left-0">
-          <img src={"/images/navector.svg"} className="w-full h-max " />
-          <div className="absolute px-6 py-4 top-0  w-full flex flex-row justify-between items-center">
+          <img src={"/images/navector.svg"} className="w-full h-60" />
+          <div className="absolute px-6 py-4 top-0 w-full flex flex-row justify-between items-center">
             <img src={"/images/logo.svg"} className="w-24" />
             <div
               onClick={() => {}}
