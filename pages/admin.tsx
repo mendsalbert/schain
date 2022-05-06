84e0bdabc48fb2876d78048201a188e41defcf9d
graphica99 <mendsalbert@gmail.com>
2022-05-06 23:43:12 +0000

    schain ui

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,4 +1,13 @@
-import { ClipboardListIcon, ReplyIcon } from "@heroicons/react/outline";
+import {
+  AdjustmentsIcon,
+  CheckCircleIcon,
+  CheckIcon,
+  ClipboardListIcon,
+  RefreshIcon,
+  ReplyIcon,
+  SparklesIcon,
+  TruckIcon,
+} from "@heroicons/react/outline";
 import React from "react";
 
 type Props = {};
@@ -31,6 +40,7 @@ const Admin = (props: Props) => {
           </div>
 
           <div className="mt-16">
+            <p className="text-xl py-4">Order 1</p>
             <ol className="items-center sm:flex">
               <li className="relative mb-6 sm:mb-0">
                 <div className="flex items-center">
@@ -40,77 +50,105 @@ const Admin = (props: Props) => {
                   <div className="hidden sm:flex w-full bg-gray-200 h-0.5 "></div>
                 </div>
                 <div className="mt-3 sm:pr-8">
-                  <h3 className="text-lg font-semibold text-gray-900 ">
-                    Flowbite Library v1.0.0
-                  </h3>
+                  <div className="flex flex-row items-center space-x-2">
+                    <h3 className="text-lg font-semibold text-gray-900 ">
+                      Order
+                    </h3>
+                    <CheckCircleIcon className="h-6" />
+                  </div>
                   <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                    Released on December 2, 2021
+                    Order made on December 2, 2021 8:00PM
                   </time>
                   <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-                    Get started with dozens of web components and interactive
-                    elements.
+                    Order description
                   </p>
                 </div>
               </li>
               <li className="relative mb-6 sm:mb-0">
                 <div className="flex items-center">
-                  <div className="flex z-10 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-0 ring-white ">
-                    <svg
-                      className="w-3 h-3 text-blue-600 dark:text-blue-300"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                        clip-rule="evenodd"
-                      ></path>
-                    </svg>
+                  <div className="flex z-10 justify-center items-center w-max h-max p-2 bg-green-400 rounded-full ring-0 ring-white ">
+                    <CheckIcon className="h-8 text-white" />
                   </div>
                   <div className="hidden sm:flex w-full bg-gray-200 h-0.5 "></div>
                 </div>
                 <div className="mt-3 sm:pr-8">
-                  <h3 className="text-lg font-semibold text-gray-900 ">
-                    Flowbite Library v1.2.0
-                  </h3>
+                  <div className="flex flex-row items-center space-x-2">
+                    <h3 className="text-lg font-semibold text-gray-900 ">
+                      Approved
+                    </h3>
+                    <CheckCircleIcon className="h-6" />
+                  </div>
                   <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                    Released on December 23, 2021
+                    Order made on December 2, 2021 8:00PM
                   </time>
                   <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-                    Get started with dozens of web components and interactive
-                    elements.
+                    Order description
                   </p>
                 </div>
               </li>
               <li className="relative mb-6 sm:mb-0">
                 <div className="flex items-center">
-                  <div className="flex z-10 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-0 ring-white ">
-                    <svg
-                      className="w-3 h-3 text-blue-600 dark:text-blue-300"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                        clip-rule="evenodd"
-                      ></path>
-                    </svg>
+                  <div className="flex z-10 justify-center items-center w-max h-max p-2 bg-gray-400 rounded-full ring-0 ring-white ">
+                    <SparklesIcon className="h-8 text-white" />
                   </div>
                   <div className="hidden sm:flex w-full bg-gray-200 h-0.5 "></div>
                 </div>
                 <div className="mt-3 sm:pr-8">
-                  <h3 className="text-lg font-semibold text-gray-900 ">
-                    Flowbite Library v1.3.0
-                  </h3>
+                  <div className="flex flex-row items-center space-x-2">
+                    <h3 className="text-lg font-semibold text-gray-900 ">
+                      Production
+                    </h3>
+                    <RefreshIcon className="h-6" />
+                  </div>
                   <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                    Released on January 5, 2022
+                    Order made on December 2, 2021 8:00PM
                   </time>
                   <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-                    Get started with dozens of web components and interactive
-                    elements.
+                    Order description
+                  </p>
+                </div>
+              </li>
+              <li className="relative mb-6 sm:mb-0">
+                <div className="flex items-center">
+                  <div className="flex z-10 justify-center items-center w-max h-max p-2 bg-gray-400 rounded-full ring-0 ring-white ">
+                    <AdjustmentsIcon className="h-8 text-white" />
+                  </div>
+                  <div className="hidden sm:flex w-full bg-gray-200 h-0.5 "></div>
+                </div>
+                <div className="mt-3 sm:pr-8">
+                  <div className="flex flex-row items-center space-x-2">
+                    <h3 className="text-lg font-semibold text-gray-900 ">
+                      Testing
+                    </h3>
+                    <RefreshIcon className="h-6" />
+                  </div>
+                  <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                    Order made on December 2, 2021 8:00PM
+                  </time>
+                  <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+                    Order description
+                  </p>
+                </div>
+              </li>
+              <li className="relative mb-6 sm:mb-0">
+                <div className="flex items-center">
+                  <div className="flex z-10 justify-center items-center w-max h-max p-2 bg-gray-400 rounded-full ring-0 ring-white ">
+                    <TruckIcon className="h-8 text-white" />
+                  </div>
+                  <div className="hidden sm:flex w-full bg-gray-200 h-0.5 "></div>
+                </div>
+                <div className="mt-3 sm:pr-8">
+                  <div className="flex flex-row items-center space-x-2">
+                    <h3 className="text-lg font-semibold text-gray-900 ">
+                      Shipping
+                    </h3>
+                    <RefreshIcon className="h-6" />
+                  </div>
+                  <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                    Order made on December 2, 2021 8:00PM
+                  </time>
+                  <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+                    Order description
                   </p>
                 </div>
               </li>
