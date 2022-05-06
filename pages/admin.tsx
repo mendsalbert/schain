d4867796666755f2060be4c3871140399b6b7017
graphica99 <mendsalbert@gmail.com>
2022-05-06 22:56:48 +0000

    schain ui

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -3,7 +3,14 @@ import React from "react";
 type Props = {};
 
 const Admin = (props: Props) => {
-  return <div>Admin</div>;
+  return (
+    <div className="font-Montserrat w-full">
+      <div className="flex flex-row w-full">
+        <div className="bg-red-500 w-2/12 h-full"></div>
+        <div className="bg-green-500 w-full h-full"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Admin;
