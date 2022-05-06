3dbb2635e9560ec29db6f89d7ef3bfbf89d8c46f
graphica99 <mendsalbert@gmail.com>
2022-05-06 18:30:56 +0000

    schain ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ const Index = (props: Props) => {
         <div className="fixed w-full left-0 -top-2 md:-top-3 z-50">
           <img src={"/images/navector.svg"} className="w-full " />
           <div className="absolute px-4 sm:px-8 md:px-12 py-5 md:py-10  top-0 w-full flex flex-row justify-between items-center">
-            <img src={"/images/logo.svg"} className="w-24" />
+            <img src={"/images/logo.svg"} className="w-24 md:w-32" />
             <div
               onClick={() => {}}
               className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500  text-center w-max   px-6 py-2 rounded-full cursor-pointer text-white"
@@ -33,7 +33,7 @@ const Index = (props: Props) => {
             <p className="text-left font-extrabold text-3xl italic text-[#0469A1]">
               Logistics Supply chain on Blockchain
             </p>
-            <div className="bg-gradient-to-r italic from-[#0469A1] via-[#0469A1] mt-2 to-[#0C9FF2]  text-center w-max  px-6 py-2 md:py-4 rounded-full cursor-pointer text-white">
+            <div className="bg-gradient-to-r italic from-[#0469A1] via-[#0469A1] mt-2 to-[#0C9FF2]  text-center w-max  px-6 py-2 md:px-8 md:py-4 rounded-full cursor-pointer text-white">
               Read More
             </div>
           </div>
@@ -48,7 +48,7 @@ const Index = (props: Props) => {
             <p className="text-left font-extrabold text-3xl italic text-[#0469A1]">
               Make an order, sit and watch the supply chain processes
             </p>
-            <div className="bg-gradient-to-r italic from-[#0469A1] via-[#0469A1] mt-2 to-[#0C9FF2]  text-center w-max   px-6 py-2 rounded-full cursor-pointer text-white">
+            <div className="bg-gradient-to-r italic from-[#0469A1] via-[#0469A1] mt-2 to-[#0C9FF2]  text-center w-max   px-6 py-2 md:px-8 md:py-4 rounded-full cursor-pointer text-white">
               Make Order
             </div>
           </div>
