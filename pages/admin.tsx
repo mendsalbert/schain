d6830e650ad5ecdb8508817f350d5f0b87e909ba
graphica99 <mendsalbert@gmail.com>
2022-05-06 23:23:57 +0000

    schain ui

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -7,8 +7,8 @@ const Admin = (props: Props) => {
   return (
     <div className="font-Montserrat  w-full">
       <div className="flex flex-row  w-full">
-        <div className="bg-gradient-to-b mt-6 space-y-10 flex p-4 flex-col text-center items-center from-[#0469A1] via-[#0469A1]  to-[#0C9FF2]  h-screen">
-          <img src={"/images/logo.svg"} className="w-28" />
+        <div className="bg-gradient-to-b  space-y-10 flex p-4 flex-col text-center items-center from-[#0469A1] via-[#0469A1]  to-[#0C9FF2]  h-screen">
+          <img src={"/images/logo.svg"} className="w-28 mt-6" />
           <div className="bg-[#ffffff81] p-4 rounded-lg">
             <ClipboardListIcon className="h-10 text-white" />
           </div>
@@ -18,7 +18,10 @@ const Admin = (props: Props) => {
         </div>
         <div className="w-full p-4 mt-6 h-screen">
           <div className="flex flex-row justify-between">
-            <p>account div</p>
+            <div>
+              <div className="bg-gray-500 rounded-md h-4 w-4"></div>
+              <div></div>
+            </div>
             <div className="bg-gradient-to-r  from-[#0469A1] via-[#0469A1]  to-[#0C9FF2]  text-center w-max   px-6 py-2  rounded-lg cursor-pointer text-white">
               Make Order
             </div>
