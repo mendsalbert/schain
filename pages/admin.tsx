f199fd9faa05a313f4101fe0384eb9c683187166
graphica99 <mendsalbert@gmail.com>
2022-05-06 23:16:28 +0000

    schain ui

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,4 +1,4 @@
-import { ClipboardListIcon } from "@heroicons/react/outline";
+import { ClipboardListIcon, ReplyIcon } from "@heroicons/react/outline";
 import React from "react";
 
 type Props = {};
@@ -7,11 +7,13 @@ const Admin = (props: Props) => {
   return (
     <div className="font-Montserrat  w-full">
       <div className="flex flex-row w-full">
-        <div className="bg-gradient-to-b flex flex-col text-center items-center from-[#0469A1] via-[#0469A1]  to-[#0C9FF2]  h-screen">
-          {/* <img src={"/images/logo.svg"} /> */}
+        <div className="bg-gradient-to-b space-y-10 flex p-4 flex-col text-center items-center from-[#0469A1] via-[#0469A1]  to-[#0C9FF2]  h-screen">
+          <img src={"/images/logo.svg"} className="w-24" />
           <div className="">
             <ClipboardListIcon className="h-10 text-white" />
-            <p>Orders</p>
+          </div>
+          <div className="">
+            <ReplyIcon className="h-8 text-white" />
           </div>
         </div>
         <div className="bg-green-500 w-full h-screen">fsdfsdf</div>
