b9ac9f00a8d0bc2138485d6d1816833094a96bce
graphica99 <mendsalbert@gmail.com>
2022-05-06 23:49:56 +0000

    schain ui

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -16,7 +16,7 @@ const Admin = (props: Props) => {
   return (
     <div className="font-Montserrat  w-full">
       <div className="flex flex-row  w-full">
-        <div className="bg-gradient-to-b  space-y-10 flex p-4 flex-col text-center items-center from-[#0469A1] via-[#0469A1]  to-[#0C9FF2]  h-screen">
+        <div className="bg-gradient-to-b  space-y-10 flex p-4 flex-col text-center items-center from-[#0469A1] via-[#0469A1]  to-[#0C9FF2]  ">
           <img src={"/images/logo.svg"} className="w-28 mt-6" />
           <div className="bg-[#ffffff81] p-4 rounded-lg">
             <ClipboardListIcon className="h-10 text-white" />
@@ -28,7 +28,8 @@ const Admin = (props: Props) => {
         <div className="w-full px-10 mt-10 h-screen">
           <div className="flex flex-row justify-between">
             <div className="flex flex-row justify-center items-center space-x-2 ">
-              <div className="bg-gray-500 rounded-md h-10 w-10"></div>
+              <img src="/images/profile.jpg" className="w-8" />
+
               <div>
                 <p>0x5ffdsef..455fad</p>
                 <p>User</p>
