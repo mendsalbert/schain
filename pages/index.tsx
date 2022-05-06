1d38cd64a135d1d6a9135b01d1ecbd79c403b032
graphica99 <mendsalbert@gmail.com>
2022-05-06 14:23:00 +0000

    schain ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,12 +7,11 @@ const Index = (props: Props) => {
     <div>
       {/* layout */}
       <div className="font-Montserrat mx-6 relative">
-        <div>
-          <img
-            src={"/images/navector.svg"}
-            className="w-full
-    "
-          />
+        <div className="fixed top-0 left-0">
+          <img src={"/images/navector.svg"} className="w-full " />
+          <div className="fixed mx-6 top-0 flex flex-row">
+            <p>fdsdf</p>
+          </div>
         </div>
       </div>
     </div>
