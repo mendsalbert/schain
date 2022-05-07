d8ad3ff0e63edf5e31e6d971c8386cf8d7b8fb79
graphica99 <mendsalbert@gmail.com>
2022-05-07 13:50:38 +0000

    schain ui

--- components/partials/Footer.jsx
+++ components/partials/Footer.jsx
@@ -196,12 +196,11 @@ function Footer() {
 
           {/* Copyrights note */}
           <div className="text-sm text-gray-600 mr-4">
-            Made by{" "}
             <a
               className="text-blue-600 hover:underline"
               href="https://cruip.com/"
             >
-              Cruip
+              @schain 2022
             </a>
             . All rights reserved.
           </div>
