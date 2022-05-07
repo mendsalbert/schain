2dd76a88fdce8443b5929ef9a160304a1347632c
graphica99 <mendsalbert@gmail.com>
2022-05-07 13:46:06 +0000

    schain ui

--- components/partials/Testimonials.jsx
+++ components/partials/Testimonials.jsx
@@ -45,7 +45,7 @@ function Testimonials() {
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
             <h2 className="h2 mb-4">
-              Accepts by over 500 cryptocurrencies all over the world
+              Accepted by over 500 cryptocurrencies all over the world
             </h2>
             <p className="text-xl text-gray-600" data-aos="zoom-y-out">
               Order payment can be made in any cryptocurrency of the users
