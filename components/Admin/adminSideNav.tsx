b9988d080fb9a6c8af7ac870eb79503161a0195f
graphica99 <mendsalbert@gmail.com>
2022-05-07 18:27:17 +0000

    schain ui

--- components/Admin/adminSideNav.tsx
+++ components/Admin/adminSideNav.tsx
@@ -4,8 +4,8 @@ type Props = {};
 
 const AdminSideNav = (props: Props) => {
   return (
-    <div className="bg-gradient-to-b hidden  space-y-10 md:flex p-4 flex-col text-center items-center from-[#0469A1] via-[#0469A1]  to-[#0C9FF2]  ">
-      <img src={"/images/logo.svg"} className="w-28 mt-6" />
+    <div className="bg-gradient-to-b hidden  space-y-10 md:flex p-1 flex-col text-center items-center from-[#0469A1] via-[#0469A1]  to-[#0C9FF2]  ">
+      <img src={"/images/logo_2.svg"} className="w-28 mt-6" />
       <div className="bg-[#ffffff81] p-1 md:p-4 rounded-lg">
         <ClipboardListIcon className="h-10 text-white" />
       </div>
