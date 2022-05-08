2fc55f0e4e08ec5859c42bc47ef949782f4ba7f2
graphica99 <mendsalbert@gmail.com>
2022-05-08 18:44:39 +0000

    schain ui

--- components/partials/Testimonials.jsx
+++ components/partials/Testimonials.jsx
@@ -54,7 +54,7 @@ function Testimonials() {
           </div>
 
           {/* Items */}
-          <div className="max-w-sm md:max-w-4xl mx-auto grid gap-2 grid-cols-4 md:grid-cols-4">
+          <div className="max-w-sm md:max-w-4xl mx-auto grid gap-2 grid-cols-4 md:grid-cols-5">
             {/* Item */}
             <div className="flex items-center justify-center py-2 col-span-2 md:col-auto">
               <svg
@@ -75,12 +75,35 @@ function Testimonials() {
             {/* Item */}
             <div className="flex items-center justify-center py-2 col-span-2 md:col-auto">
               <svg
+                className="max-w-full fill-current text-gray-400"
+                clip-rule="evenodd"
+                fill-rule="evenodd"
+                stroke-linejoin="round"
+                width="82px"
+                height="82px"
+                stroke-miterlimit="1.41421"
+                viewBox="0 0 460 400"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g transform="matrix(7.5 0 0 7.5 130 50)">
+                  <circle cx="20" cy="20" fill="#00000" r="20" />
+                  <path
+                    d="m21.9 17.6-.6 3.2 5.7.8-.4 1.5-5.6-.8c-.4 1.3-.6 2.7-1.1 3.9-.5 1.4-1 2.8-1.6 4.1-.8 1.7-2.2 2.9-4.1 3.2-1.1.2-2.3.1-3.2-.6-.3-.2-.6-.6-.6-.9 0-.4.2-.9.5-1.1.2-.1.7 0 1 .1.3.3.6.7.8 1.1.6.8 1.4.9 2.2.3.9-.8 1.4-1.9 1.7-3 .6-2.4 1.2-4.7 1.7-7.1v-.4l-5.3-.8.2-1.5 5.5.8.7-3.1-5.7-.9.2-1.6 5.9.8c.2-.6.3-1.1.5-1.6.5-1.8 1-3.6 2.2-5.2s2.6-2.6 4.7-2.5c.9 0 1.8.3 2.4 1 .1.1.3.3.3.5 0 .4 0 .9-.3 1.2-.4.3-.9.2-1.3-.2-.3-.3-.5-.6-.8-.9-.6-.8-1.5-.9-2.2-.2-.5.5-1 1.2-1.3 1.9-.7 2.1-1.2 4.3-1.9 6.5l5.5.8-.4 1.5z"
+                    fill="#fff"
+                  />
+                </g>
+              </svg>
+            </div>
+
+            {/* Item */}
+            <div className="flex items-center justify-center py-2 col-span-2 md:col-auto">
+              <svg
+                className="max-w-full fill-current text-gray-400"
                 width="58"
                 height="58"
                 viewBox="0 0 48 48"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
-                className="max-w-full fill-current text-gray-400"
               >
                 <path
                   fill-rule="evenodd"
