d21f2ca09d5602d2cbd6baf040ff95d24e3d70d4
graphica99 <mendsalbert@gmail.com>
2022-05-09 19:20:52 +0000

    schain ui

--- pages/dashboard/customer/index.jsx
+++ pages/dashboard/customer/index.jsx
@@ -15,53 +15,55 @@ function Dashboard() {
   const [open, setOpen] = useState(false);
   const [comp, setComp] = useState("");
   return (
-    <div className="flex h-screen overflow-hidden font-Montserrat">
-      {/* Sidebar */}
-      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+    <>
+      <div className="flex h-screen overflow-hidden font-Montserrat">
+        {/* Sidebar */}
+        <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
-      {/* Content area */}
-      <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
-        {/*  Site header */}
-        <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+        {/* Content area */}
+        <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+          {/*  Site header */}
+          <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
-        <main>
-          <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
-            {/* Welcome banner */}
-            <WelcomeBanner />
-            {/* Cards */}
-            <div className="sm:flex sm:justify-end sm:items-center mb-8">
-              <div className="grid grid-flow-col sm:auto-cols-max justify-end sm:justify-end gap-2">
-                <button
-                  onClick={() => {
-                    setOpen(!open);
+          <main>
+            <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
+              {/* Welcome banner */}
+              <WelcomeBanner />
+              {/* Cards */}
+              <div className="sm:flex sm:justify-end sm:items-center mb-8">
+                <div className="grid grid-flow-col sm:auto-cols-max justify-end sm:justify-end gap-2">
+                  <button
+                    onClick={() => {
+                      setOpen(!open);
 
-                    setComp(<OrderModal />);
-                  }}
-                  className="px-4 py-2 flex flex-row items-center justify-center bg-indigo-500 rounded-md hover:bg-indigo-600 text-white"
-                >
-                  <svg
-                    className="w-4 h-4 fill-current opacity-50 shrink-0"
-                    viewBox="0 0 16 16"
+                      setComp(<OrderModal />);
+                    }}
+                    className="px-4 py-2 flex flex-row items-center justify-center bg-indigo-500 rounded-md hover:bg-indigo-600 text-white"
                   >
-                    <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
-                  </svg>
-                  <span className=" xs:block ml-2">Add Order</span>
-                </button>
+                    <svg
+                      className="w-4 h-4 fill-current opacity-50 shrink-0"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
+                    </svg>
+                    <span className=" xs:block ml-2">Add Order</span>
+                  </button>
+                </div>
+              </div>
+              <div className="grid grid-cols-12 gap-6">
+                <OrdersCard />
+                <OrdersPendingCard />
+                <OrderCancelCard />
+                <Orders />
               </div>
             </div>
-            <div className="grid grid-cols-12 gap-6">
-              <OrdersCard />
-              <OrdersPendingCard />
-              <OrderCancelCard />
-              <Orders />
-            </div>
-          </div>
-        </main>
-        <Modal open={open} onClose={() => setOpen(false)}>
-          {comp}
-        </Modal>
+          </main>
+        </div>
       </div>
-    </div>
+      <Modal open={open} onClose={() => setOpen(false)}>
+        {comp}
+      </Modal>
+    </>
   );
 }
 
