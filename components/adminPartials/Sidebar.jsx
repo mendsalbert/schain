f1af8082319fa6a9d4ace4db9d1b2c9cf539d575
graphica99 <mendsalbert@gmail.com>
2022-05-09 11:53:03 +0000

    schain ui

--- components/adminPartials/Sidebar.jsx
+++ components/adminPartials/Sidebar.jsx
@@ -143,42 +143,15 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
         <div className="space-y-8">
           {/* Pages group */}
           <div>
-            <h3 className="text-xs uppercase text-slate-500 font-semibold pl-3">
-              <span
-                className="hidden lg:block lg:sidebar-expanded:hidden 2xl:hidden text-center w-6"
-                aria-hidden="true"
-              >
-                •••
-              </span>
-              <span className="lg:hidden lg:sidebar-expanded:block 2xl:block">
-                Pages
-              </span>
-            </h3>
             <ul className="mt-3">
               {/* Dashboard */}
-              <li
-                className={`px-3 py-2 rounded-sm mb-0.5 last:mb-0 ${"bg-slate-900"}`}
-              >
+              <li className={`px-3 py-2 rounded-sm mb-0.5 last:mb-0 `}>
                 <div
                   end
                   to="/"
                   className={`block text-slate-200 hover:text-white truncate transition duration-150 ${"hover:text-slate-200"}`}
                 >
                   <div className="flex items-center">
-                    <svg className="shrink-0 h-6 w-6" viewBox="0 0 24 24">
-                      <path
-                        className={`fill-current text-slate-400 ${"!text-indigo-500"}`}
-                        d="M12 0C5.383 0 0 5.383 0 12s5.383 12 12 12 12-5.383 12-12S18.617 0 12 0z"
-                      />
-                      <path
-                        className={`fill-current text-slate-600 ${"text-indigo-600"}`}
-                        d="M12 3c-4.963 0-9 4.037-9 9s4.037 9 9 9 9-4.037 9-9-4.037-9-9-9z"
-                      />
-                      <path
-                        className={`fill-current text-slate-400 ${"text-indigo-200"}`}
-                        d="M12 15c-1.654 0-3-1.346-3-3 0-.462.113-.894.3-1.285L6 6l4.714 3.301A2.973 2.973 0 0112 9c1.654 0 3 1.346 3 3s-1.346 3-3 3z"
-                      />
-                    </svg>
                     <span className="text-sm font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
                       Dashboard
                     </span>
