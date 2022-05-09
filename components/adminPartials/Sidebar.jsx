ea01cdaf2777d8a9b7611eabf45e58101eb49886
graphica99 <mendsalbert@gmail.com>
2022-05-09 11:49:21 +0000

    schain ui

--- components/adminPartials/Sidebar.jsx
+++ components/adminPartials/Sidebar.jsx
@@ -58,8 +58,9 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
   return (
     <div>
       {/* Sidebar backdrop (mobile only) */}
+
       <div
-        className={`fixed inset-0  bg-slate-900 bg-opacity-30 z-40 lg:hidden lg:z-auto transition-opacity duration-200 ${
+        className={`fixed inset-0  bg-opacity-30 z-40 lg:hidden lg:z-auto transition-opacity duration-200 ${
           sidebarOpen ? "opacity-100" : "opacity-0 pointer-events-none"
         }`}
         aria-hidden="true"
@@ -69,7 +70,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
       <div
         id="sidebar"
         ref={sidebar}
-        className={`flex flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 transform h-screen overflow-y-scroll lg:overflow-y-auto no-scrollbar w-64 lg:w-20 lg:sidebar-expanded:!w-64 2xl:!w-64 shrink-0 bg-slate-800 p-4 transition-all duration-200 ease-in-out ${
+        className={`flex flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 transform h-screen overflow-y-scroll lg:overflow-y-auto no-scrollbar w-64 lg:w-20 lg:sidebar-expanded:!w-64 2xl:!w-64 shrink-0  bg-gradient-to-b  from-[#0469A1] via-[#0469A1]  to-[#0C9FF2] p-4 transition-all duration-200 ease-in-out ${
           sidebarOpen ? "translate-x-0" : "-translate-x-64"
         }`}
       >
