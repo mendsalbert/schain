7ab2feff05509b916431d11bab5edcc13446a283
graphica99 <mendsalbert@gmail.com>
2022-05-10 15:55:11 +0000

    schain ui

---
+++ pages/dashboard/manager/index.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useRef, useEffect } from "react";
+
+import Sidebar from "../../../components/adminPartials/Sidebar";
+import Header from "../../../components/adminPartials/Header";
+import WelcomeBanner from "../../../components/adminPartials/dashboard/WelcomeBanner";
+import Orders from "../../../components/adminPartials/dashboard/Orders";
+import OrdersCard from "../../../components/adminPartials/dashboard/OdersCards";
+import OrdersPendingCard from "../../../components/adminPartials/dashboard/OrdersPendingCard";
+import OrderCancelCard from "../../../components/adminPartials/dashboard/OrderCancelCard";
+import Modal from "../../../components/Modal";
+import OrderModal from "../../../components/OrderModal.jsx";
+import AdminAuthModal from "../../../components/AdminAuthModal.jsx";
+import UsersCard from "../../../components/adminPartials/dashboard/UsersCard";
+import UserRoles from "../../../components/adminPartials/dashboard/UserRoles";
+function Dashboard() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [comp, setComp] = useState("");
+
+  useEffect(() => {
+    let managerAddr = localStorage.getItem("managerAddr");
+    if (managerAddr !== "0x0") {
+      window.location.href = "/";
+    }
+  }, []);
+
+  return (
+    <>
+      <div className="flex h-screen overflow-hidden font-Montserrat">
+        {/* Sidebar */}
+
+        <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+
+        {/* Content area */}
+        <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+          {/*  Site header */}
+          <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+
+          <main>
+            <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
+              {/* Welcome banner */}
+              <WelcomeBanner />
+              {/* Cards */}
+
+              <div className="grid grid-cols-12 gap-6">
+                <OrdersCard />
+                <UserRoles />
+              </div>
+            </div>
+          </main>
+        </div>
+      </div>
+      {/* if not authenticated? return false else setOpen is true */}
+      <Modal open={open} onClose={() => setOpen(!auth ? true : false)}>
+        {comp}
+      </Modal>
+    </>
+  );
+}
+
+export default Dashboard;
