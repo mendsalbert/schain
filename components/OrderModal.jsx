e54039dae3223d498857d3ac6fa1ab489f88b120
graphica99 <mendsalbert@gmail.com>
2022-05-10 09:28:25 +0000

    schain ui

--- components/OrderModal.jsx
+++ components/OrderModal.jsx
@@ -2,87 +2,12 @@ import React, { useState, useEffect } from "react";
 import { productData } from "../utils/sample-data";
 
 const OrderModal = (props) => {
-  const [image, setimage] = useState("");
+  const [quantity, setquantity] = useState(1);
   const [productname, setproductname] = useState("");
 
   const filterImage = productData.filter((p) => p.name === productname);
-  console.log(filterImage);
-  const [formInput, updateFormInput] = useState({
-    title: "",
-    description: "",
-    category: "",
-    targetAmount: "",
-    endDate: "",
-  });
 
   return (
-    // <div className="p-5 font-Montserrat overflow-auto">
-    //   <p className="text-center text-gray-500 text-lg">Add Order</p>
-    //   <div className="py-3 space-y-3">
-    //     <input
-    //       type="text"
-    //       required={true}
-    //       onChange={(e) =>
-    //         updateFormInput({ ...formInput, title: e.target.value })
-    //       }
-    //       className="w-full py-3 outline-none ring-2 rounded-lg p-3"
-    //       placeholder="Enter title "
-    //     />
-    //     <input
-    //       type="text"
-    //       required={true}
-    //       onChange={(e) =>
-    //         updateFormInput({ ...formInput, description: e.target.value })
-    //       }
-    //       className="w-full py-3 outline-none ring-2 rounded-lg p-3"
-    //       placeholder="Enter description"
-    //     />
-    //     <input
-    //       type="number"
-    //       onChange={(e) =>
-    //         updateFormInput({ ...formInput, targetAmount: e.target.value })
-    //       }
-    //       className="w-full py-3 outline-none ring-2 rounded-lg p-3"
-    //       placeholder="Targeted Amount (ETH)"
-    //       required={true}
-    //     />
-    //     <div>
-    //       <label className="mt-6">Select category</label>
-    //       <select
-    //         placeholder="Select description"
-    //         required={true}
-    //         onChange={(e) =>
-    //           updateFormInput({ ...formInput, category: e.target.value })
-    //         }
-    //         className="w-full py-3 outline-none ring-2 rounded-lg p-3"
-    //       >
-    //         <option value="education"></option>
-    //         <option value="education">Education</option>
-    //         <option value="disaster">Disaster</option>
-    //         <option value="health">Health</option>
-    //         <option value="famine">Famine</option>
-    //       </select>
-    //     </div>
-    //     <div className="">
-    //       <label className="mt-6">End date</label>
-    //       <input
-    //         required={true}
-    //         type="date"
-    //         onChange={(e) =>
-    //           updateFormInput({ ...formInput, endDate: e.target.value })
-    //         }
-    //         className="w-full py-3 outline-none ring-2 rounded-lg p-3"
-    //         placeholder="Enter amount manually"
-    //       />
-    //     </div>
-
-    //     <div className="bg-gradient-to-r from-cyan-500 to-blue-500 px-6 py-3 rounded-lg text-center cursor-pointer text-white">
-    //       Continue
-    //     </div>
-    //   </div>
-    // </div>
-
-    // <form className="w-full ">
     <>
       <p className=" text-gray-500 uppercase text-md pb-4 font-bold">
         Add Order
@@ -124,6 +49,8 @@ const OrderModal = (props) => {
             className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
             id="grid-last-name"
             type="number"
+            value={quantity}
+            onChange={(e) => setquantity(e.target.value)}
             placeholder="quantity"
           />
         </div>
@@ -134,17 +61,27 @@ const OrderModal = (props) => {
       <div className="flex flex-wrap -mx-3 mb-6">
         <div className="w-full px-3">
           <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-            Password
+            Address Line 1
           </label>
           <input
             className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
             id="grid-password"
-            type="password"
-            placeholder="******************"
+            type="text"
+            placeholder="address"
           />
-          <p className="text-gray-600 text-xs italic">
-            Make it as long and as crazy as you'd like
-          </p>
+        </div>
+      </div>
+      <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="w-full px-3">
+          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+            Phone Number
+          </label>
+          <input
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="grid-password"
+            type="text"
+            placeholder="+233 ********"
+          />
         </div>
       </div>
       <div className="flex flex-wrap -mx-3 mb-2">
@@ -156,32 +93,19 @@ const OrderModal = (props) => {
             className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
             id="grid-city"
             type="text"
-            placeholder="Albuquerque"
+            placeholder="Accra"
           />
         </div>
         <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
           <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
             State
           </label>
-          <div className="relative">
-            <select
-              className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="grid-state"
-            >
-              <option>New Mexico</option>
-              <option>Missouri</option>
-              <option>Texas</option>
-            </select>
-            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-              <svg
-                className="fill-current h-4 w-4"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-              >
-                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-              </svg>
-            </div>
-          </div>
+          <input
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="grid-city"
+            type="text"
+            placeholder="Tema"
+          />
         </div>
         <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
           <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
@@ -197,7 +121,7 @@ const OrderModal = (props) => {
       </div>
       <div className="flex items-start">
         <div className="text-xl font-bold text-slate-800 mr-2">
-          Total cost : $50 - 0.00041ETH
+          Total cost : ${filterImage[0].price * quantity} - 0.00041ETH
         </div>
       </div>
       <div className="flex flex-wrap -mx-3 mb-6 mt-4">
