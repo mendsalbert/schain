8fa665094fb026c9de154d002128c8f2f3bee062
graphica99 <mendsalbert@gmail.com>
2022-05-10 12:18:30 +0000

    schain ui

--- components/AdminAuthModal.tsx
+++ components/AdminAuthModal.tsx
@@ -1,9 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { productData } from "../utils/sample-data";
-
+import { uName, uPassword } from "../auth";
 const AdminAuthModal = (props) => {
   const [username, setusername] = useState("");
   const [password, setpassword] = useState("");
+
+  const onAuthenticateHandler = () => {
+    if (username === uName && password === uPassword) {
+      console.log("authenticated");
+      localStorage.setItem("auth", true);
+    } else {
+      alert("wrong password and username combination");
+      console.log("not authenticated");
+    }
+  };
   return (
     <>
       <div className="p-5 font-Montserrat overflow-auto">
@@ -29,7 +39,12 @@ const AdminAuthModal = (props) => {
             }}
           />
 
-          <div className="bg-gradient-to-r from-cyan-500 to-blue-500 px-6 py-3 rounded-lg text-center cursor-pointer text-white">
+          <div
+            onClick={() => {
+              onAuthenticateHandler();
+            }}
+            className="bg-gradient-to-r from-cyan-500 to-blue-500 px-6 py-3 rounded-lg text-center cursor-pointer text-white"
+          >
             Log In
           </div>
         </div>
