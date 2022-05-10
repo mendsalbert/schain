bbde0912417d24c9c44aceaee6be86854b74a67a
graphica99 <mendsalbert@gmail.com>
2022-05-10 09:23:24 +0000

    schain ui

--- components/OrderModal.jsx
+++ components/OrderModal.jsx
@@ -100,6 +100,7 @@ const OrderModal = (props) => {
               className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               id="grid-state"
             >
+              <option>Select Product</option>
               {productData.map((product) => (
                 <option value={product.name}>{product.name}</option>
               ))}
