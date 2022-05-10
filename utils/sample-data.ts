55e741285a28de25fd44f9055c3057d7f02b147f
graphica99 <mendsalbert@gmail.com>
2022-05-10 07:01:44 +0000

    schain ui

--- utils/sample-data.ts
+++ utils/sample-data.ts
@@ -1,9 +1,54 @@
-import { User } from '../interfaces'
+import { User } from "../interfaces";
 
 /** Dummy user data. */
 export const sampleUserData: User[] = [
-  { id: 101, name: 'Alice' },
-  { id: 102, name: 'Bob' },
-  { id: 103, name: 'Caroline' },
-  { id: 104, name: 'Dave' },
-]
+  { id: 101, name: "Alice" },
+  { id: 102, name: "Bob" },
+  { id: 103, name: "Caroline" },
+  { id: 104, name: "Dave" },
+];
+
+export const productData = [
+  {
+    id: 1,
+    name: "Cerelac",
+    price: 20.5,
+    imageUrl: "/images/cerelac.png",
+  },
+  {
+    id: 2,
+    name: "Milo",
+    price: 30.7,
+    imageUrl: "/images/milo.png",
+  },
+  {
+    id: 3,
+    name: "Milkybar",
+    price: 25.1,
+    imageUrl: "/images/milkybar.png",
+  },
+  {
+    id: 4,
+    name: "Maggi",
+    price: 47.6,
+    imageUrl: "/images/maggi.png",
+  },
+  {
+    id: 5,
+    name: "Lion Cereals",
+    price: 50.2,
+    imageUrl: "/images/lionCereals.png",
+  },
+  {
+    id: 6,
+    name: "Kitkat",
+    price: 17.8,
+    imageUrl: "/images/kitkat.png",
+  },
+  {
+    id: 7,
+    name: "Bakers",
+    price: 15.0,
+    imageUrl: "/images/bakers.png",
+  },
+];
