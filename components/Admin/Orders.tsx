151570b63cabcf7c807cba01ba01b3ca890f193a
graphica99 <mendsalbert@gmail.com>
2022-05-13 06:05:12 +0000

    schain ui

--- components/Admin/Orders.tsx
+++ components/Admin/Orders.tsx
@@ -11,12 +11,14 @@ import {
 } from "@heroicons/react/outline";
 import Modal from "../Modal";
 import ReviewModal from "../ReviewModal";
+import { timeConverter } from "../../lib/utilities";
 function Orders({ order }) {
   const [open, setOpen] = useState(false);
   const [comp, setComp] = useState("") as any;
 
   type orderType = {
     product?: string;
+    orderdate?: any;
     confirmed?: boolean;
     confirmdate?: any;
     produced?: any;
@@ -52,7 +54,13 @@ function Orders({ order }) {
             <ol className="items-center sm:flex">
               <li className="relative mb-6  sm:mb-0">
                 <div className="flex items-center">
-                  <div className="flex z-10 justify-center items-center w-max h-max p-2 bg-green-400 rounded-full ring-0 ring-white ">
+                  <div
+                    className={`flex z-10 justify-center items-center w-max h-max p-2 ${
+                      order.orderdate.toString() > 1
+                        ? "bg-green-400"
+                        : "bg-gray-400"
+                    } rounded-full ring-0 ring-white`}
+                  >
                     <ClipboardListIcon className="h-8 text-white" />
                   </div>
                   <div className="hidden sm:flex w-full bg-gray-200 h-0.5 "></div>
@@ -62,29 +70,45 @@ function Orders({ order }) {
                     <h3 className="text-lg font-semibold text-gray-900 ">
                       Order
                     </h3>
-                    <CheckCircleIcon className="h-6" />
+                    {order.orderdate.toString() > 1 ? (
+                      <CheckCircleIcon className="h-6" />
+                    ) : (
+                      <RefreshIcon className="h-6" />
+                    )}
                   </div>
                   <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                    Order made on December 2, 2021 8:00PM
+                    Order made on {timeConverter(order.orderdate.toString())}
                   </time>
                 </div>
               </li>
-              <li className="relative mb-6 sm:mb-0">
+              <li className="relative mb-6  sm:mb-0">
                 <div className="flex items-center">
-                  <div className="flex z-10 justify-center items-center w-max h-max p-2 bg-green-400 rounded-full ring-0 ring-white ">
-                    <CheckIcon className="h-8 text-white" />
+                  <div
+                    className={`flex z-10 justify-center items-center w-max h-max p-2 ${
+                      order.confirmed ? "bg-green-400" : "bg-gray-400"
+                    } rounded-full ring-0 ring-white`}
+                  >
+                    <CheckCircleIcon className="h-8 text-white" />
                   </div>
                   <div className="hidden sm:flex w-full bg-gray-200 h-0.5 "></div>
                 </div>
-                <div className="mt-3 sm:pr-8">
-                  <div className="flex flex-row items-center space-x-2">
+                <div className="mt-3 group sm:pr-8">
+                  <div className="flex  flex-row items-center space-x-2">
                     <h3 className="text-lg font-semibold text-gray-900 ">
                       Approved
                     </h3>
-                    <CheckCircleIcon className="h-6" />
+                    {order.confirmed ? (
+                      <CheckCircleIcon className="h-6" />
+                    ) : (
+                      <RefreshIcon className="h-6" />
+                    )}
                   </div>
                   <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                    Order made on December 2, 2021 8:00PM
+                    {order.confirmed
+                      ? `Order made on ${timeConverter(
+                          order.confirmdate.toString()
+                        )}`
+                      : ""}
                   </time>
                 </div>
               </li>
--- lib/utilities.ts
+++ lib/utilities.ts
@@ -43,3 +43,30 @@ export function numDaysBetween(d1, d2) {
   var diff = Math.abs(d1 - d2.getTime() / 1000);
   return diff / (60 * 60 * 24);
 }
+
+export function timeConverter(UNIX_timestamp) {
+  var a = new Date(UNIX_timestamp * 1000);
+  var months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  var year = a.getFullYear();
+  var month = months[a.getMonth()];
+  var date = a.getDate();
+  var hour = a.getHours();
+  var min = a.getMinutes();
+  var sec = a.getSeconds();
+  var time =
+    date + " " + month + " " + year + " " + hour + ":" + min + ":" + sec;
+  return time;
+}
