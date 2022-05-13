c77b4e65626baf45d9eba1e9661476b901d12e05
graphica99 <mendsalbert@gmail.com>
2022-05-13 15:11:46 +0000

    schain ui

--- components/adminPartials/dashboard/UserRoles.jsx
+++ components/adminPartials/dashboard/UserRoles.jsx
@@ -1,7 +1,12 @@
 import { EyeIcon } from "@heroicons/react/outline";
-import React from "react";
+import React, { useContext, useState } from "react";
 import Link from "next/link";
+import { AuthContext } from "../../../utils/AuthProvider";
+
 function UserRoles() {
+  const { provider } = useContext(AuthContext);
+  const [role, setrole] = useState("");
+  const [address, setaddress] = useState("");
   return (
     <div className="col-span-full xl:col-span-12 bg-white shadow-lg w-full rounded-md border border-slate-200">
       <header className="px-5 py-4 border-b border-slate-100 flex flex-row items-center space-x-4">
@@ -15,18 +20,25 @@ function UserRoles() {
           className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
           id="grid-last-name"
           type="text"
+          value={address}
+          onChange={(e) => {
+            setaddress(e.target.value);
+          }}
           placeholder="0x0..fab3e"
         />
         <div className="relative">
           <select
+            onChange={(e) => {
+              setrole(e.target.value);
+            }}
             className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
             id="grid-state"
           >
             <option>Select Role</option>
-            <option>Manager</option>
-            <option>Manufacturer</option>
-            <option>Tester</option>
-            <option>Transpoter</option>
+            <option value="manager">Manager</option>
+            <option value="manufacturer">Manufacturer</option>
+            <option value="tester">Tester</option>
+            <option value="transporter">Transpoter</option>
           </select>
           <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
             <svg
