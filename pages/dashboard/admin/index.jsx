2ec646ee9206b8ccab2819b4d5c82ac1e882289a
graphica99 <mendsalbert@gmail.com>
2022-05-17 23:04:37 +0000

    schain ui

--- pages/dashboard/admin/index.jsx
+++ pages/dashboard/admin/index.jsx
@@ -98,7 +98,7 @@ function Dashboard() {
                     >
                       <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
                     </svg>
-                    <span className=" xs:block ml-2">Add Order</span>
+                    <span className=" xs:block ml-2">Add Product</span>
                   </button>
                 </div>
               </div>
